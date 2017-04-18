042fce629e5508dd5b8d957ad60ccc518219c6c4
fukumochi <fukumochi@fukumotosusumunari-no-MacBook-Pro.local>
2017-04-18 18:14:12 +0900

    top-js
    css,jsつけました。

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+
+ $(".profile_image_rina").delay(100).fadeIn(1200);
+
+ $(".profile_image_tomita").delay(300).fadeIn(1200);
+
+ $(".profile_image_fuku").delay(500).fadeIn(1200);
+
+ $(".profile_image_izumi").delay(700).fadeIn(1200);
+
+ $(".profile_image_kou").delay(900).fadeIn(1200);
+
+ $(".profile_image_hana").delay(1100).fadeIn(1200);
+
+});
